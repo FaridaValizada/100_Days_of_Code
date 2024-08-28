cd22a3f722f68f49e29d45c0e97278a6d8084dc4
FaridaValizada <farida.valizada@gmail.com>
2024-08-28 22:03:11 +0200

    Initial commit

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,21 @@
-len("Hello")
+print(len("Hello"))
+
+#Subscripting
+print("Hello"[-2])
+
+#String
+print("123" + "345")
+
+#Integer
+print(123 + 345)
+
+#Large Integers
+print(123_456_789)
+
+#Float = Floating Point Number
+print(3.14159)
+print(3141.59)
+
+#Boolean
+print(True)
+print(False)
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,10 @@
-len(12345)
+len("12345")
+
+print(type("Hello"))
+print(type(123))
+print(type(123.56))
+print(type(True))
+
+print(int("123") + int("456"))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
